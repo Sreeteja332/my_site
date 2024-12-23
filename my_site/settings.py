9c65df443e618b37fae8d5d26f76ad3acfd69da6
Sreeteja332 <sreeteja332@gmail.com>
2024-12-23 17:02:38 +0530

    14.3

--- my_site/settings.py
+++ my_site/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS=[
 ]
 
 MEDIA_ROOT= BASE_DIR / "uploads"
-MEADIA_URL = "/files/"
+MEDIA_URL = "/files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
