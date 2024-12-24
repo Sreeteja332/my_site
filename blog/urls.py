504439cc508a371f6066a151041fe0d59dc5240a
Sreeteja332 <sreeteja332@gmail.com>
2024-12-24 18:02:44 +0530

    till read later

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("",views.StartingPageView.as_view(),name="starting-page"),
     path("posts",views.AllPostsView.as_view(),name="posts-page"),
     path("posts/<slug:slug>",views.SinglePostView.as_view(),
-        name="post-detail-page")#/posts/(my-first-post)--> this concept is called slug
+        name="post-detail-page"),#/posts/(my-first-post)--> this concept is called slug
+    path("read-later",views.ReadLaterView.as_view(),name="read-later")
 ]
